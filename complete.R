28c8a803efef3666bc9c52925ab9eb90bbf922a3
Irakli Chakaberia <iraklic@gmail.com>
2015-07-17 22:10:42 -0400

    removed debug print statement

--- complete.R
+++ complete.R
@@ -1,11 +1,10 @@
 complete <- function(directory, id = 1:332)
     {
+    out <- data.frame(id = numeric(), nobs = numeric())
     for (i in id)
         {
         x <- read.csv(paste(directory, "/", str_pad(i, 3, pad = "0"), ".csv", sep = ""))
-        o <- data.frame(id = i, nobs = sum(complete.cases(x)))
-        out <- rbind(out, o, stringsAsFactors=FALSE)
+        out <- rbind(out, data.frame(id = i, nobs = sum(complete.cases(x))))
         }
     out
-    
     }
